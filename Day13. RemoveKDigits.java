7be8c42afd040b34f8c097a6dda7e7311ec15094
Chahat Bhatia <43989503+bhatiachahat@users.noreply.github.com>
2020-05-21 08:00:15 +0530

    Update Day13. RemoveKDigits.java

--- Day13. RemoveKDigits.java
+++ Day13. RemoveKDigits.java
@@ -10,6 +10,7 @@ Output: "1219"
 Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
 Example 2:
 Input: num = "10200", k = 1
+
 Output: "200"
 Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
 Example 3:
